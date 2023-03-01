e612d7f2d0d6bdfe39f3080e7c02f8c92da87c29
Oleg Chumin <tschumin.oleg@gmail.com>
2023-03-01 13:01:37 +0300

    Add new files

--- While loops/Balance checker/src/Main.kt
+++ While loops/Balance checker/src/Main.kt
@@ -2,10 +2,12 @@ import java.util.Scanner
 
 fun main() {
     val scanner = Scanner(System.`in`)
-    var balance = readln().toInt()
+    var balance = scanner.nextLine().toInt()
     var debit = balance
     while (scanner.hasNextInt()) {
-        debit = scanner.nextInt()
+        if(scanner.hasNext().equals("")) {
+            break
+        } else {debit = scanner.nextInt()}
         if (debit > balance) {
             println("Error, insufficient funds for the purchase. Your balance is $balance, but you need $debit.")
             break;
@@ -16,4 +18,5 @@ fun main() {
     if (debit < balance) {
         println("Thank you for choosing us to manage your account! Your balance is $balance.")
     }
+    scanner.close()
 }
